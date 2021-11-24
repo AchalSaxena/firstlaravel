3a329cf7d875bb2d5681f9b819dc319583b85ae1
Vijay Goswami <hexpit@gmail.com>
2021-11-24 13:21:35 +0530

    This is First Laravel  Project on Views

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\servicecontroller;
+use App\Http\Controllers\contactcontroller;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
+Route::get('skill', function () {
+    return view('skill');
+})->name('skill');
+Route::get('services',[servicecontroller::class,'show'])->name('services');
+Route::get('contact',[contactcontroller::class,'show'])->name('contact');
